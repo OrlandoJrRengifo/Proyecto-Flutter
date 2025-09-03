9a45baa529fa10a5a77efbef24f759278017d3c2
Orlando Jr. Rengifo <107599003+OrlandoJrRengifo@users.noreply.github.com>
2025-09-03 00:57:35 -0500

    Corrección de errores de integración

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'presentation/pages/loginScreen.dart';
 import 'screens/courses_screen.dart';
 import 'services/auth_service.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '/presentation/pages/categories_page.dart';
+import 'di.dart' as di;
+import 'presentation/pages/loginScreen.dart';
+
 
 void main() {
   // Intento de autentación de usuario
@@ -10,16 +14,26 @@ void main() {
 }
 
 class MainApp extends StatelessWidget {
-  final String demoCourseId = 'Programcion Movil';
+  final String demoCourseId = '101';
 
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sistema de Cursos',
-      theme: ThemeData(primarySwatch: Colors.purple, useMaterial3: true),
-      home: const CoursesScreen(),
-    );
+    //Home
+    //return MaterialApp(
+    //  title: 'Sistema de Cursos',
+    //  theme: ThemeData(primarySwatch: Colors.purple, useMaterial3: true),
+    //  home: const CoursesScreen(),
+    //);
+    //Login
+    return const MaterialApp(home: LoginScreen());
+    //CRUD Categorias
+    //return MaterialApp(
+    //  home: BlocProvider(
+    //    create: (_) => di.createCategoriesCubit(),
+    //    child: CategoriesPage(courseId: demoCourseId),
+    //  ),
+    //);
   }
 }
